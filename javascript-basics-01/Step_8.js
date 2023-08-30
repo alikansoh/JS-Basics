648d4c87a85c0490734baad95174644b82c272b7
alikansoh <alikansoh.dev@gmail.com>
2023-08-30 20:48:55 +0300

    added step_8

---
+++ javascript-basics-01/Step_8.js
@@ -0,0 +1,8 @@
+function cal(){
+    var age= parseInt(document.getElementById("age").value);
+    if(age>=18){
+        alert("you are over 18");
+    }
+    else
+        alert("you are under 18");
+}
