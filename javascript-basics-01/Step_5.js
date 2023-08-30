e98de880d2e38abd8df52b3f8c8b711e516e1dbd
alikansoh <alikansoh.dev@gmail.com>
2023-08-30 15:26:16 +0300

    added step_5

---
+++ javascript-basics-01/Step_5.js
@@ -0,0 +1,3 @@
+var num1 = parseInt (prompt("Please enter the first number:"));
+var num2 = parseInt (prompt("Please enter the second number  :"));
+alert("the sum of the 2 number is "+ parseInt(num1*num2));
