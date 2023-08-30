c73d135a4cb5ddec4ed84ff93e97c10d0462505c
alikansoh <alikansoh.dev@gmail.com>
2023-08-30 15:09:03 +0300

    added step_4

---
+++ javascript-basics-01/Step_4.js
@@ -0,0 +1,7 @@
+var userName = prompt("Please enter your name:");
+var surName = prompt("Please enter your last name :");
+var city = prompt("Please enter your city:");
+
+
+
+alert("Hello, " + userName +" "+surName +"\n your city is "+city);
