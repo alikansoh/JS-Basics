0663ca8395b10990efe5a7eabd6d420871176ac7
alikansoh <alikansoh.dev@gmail.com>
2023-08-31 09:00:37 +0300

    added added Step_1 basic 2

---
+++ javascript-basics-02/step 1/Step_1.js
@@ -0,0 +1,8 @@
+
+document.addEventListener("DOMContentLoaded", function () {
+    const image = document.querySelector("#image1");
+    const newImageSrc = "images/image1_2.jpg";
+    image.addEventListener("mouseover", function () {
+        image.src = newImageSrc;
+    });
+});
