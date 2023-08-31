b74d80b9ec7c8a987da3ef0ea14fa4a250cb94cb
alikansoh <alikansoh.dev@gmail.com>
2023-08-31 15:01:46 +0300

    added Step_4

---
+++ javascript-basics-02/step 4/Step_4.js
@@ -0,0 +1,11 @@
+
+document.addEventListener("DOMContentLoaded", function () {
+    let inputs=document.querySelectorAll("input"); 
+    const button = document.querySelector("button");
+     button.addEventListener("click", function () {
+        const confirmation = confirm("do you need to reset the form yes or no ");
+        if (confirmation) {
+            inputs.forEach(input=>{input.value=""});}
+            
+    });
+});
