fd981f86a6d25105ebb2125a453e537bf05ea043
alikansoh <alikansoh.dev@gmail.com>
2023-09-01 16:10:09 +0300

    added step_6

---
+++ javascript-basics-03/step 1/Step_1.js
@@ -0,0 +1,7 @@
+ var image = document.querySelector("#image1")
+image.addEventListener("mouseover", function () {
+    image.style.border = "3px solid red";
+});
+image.addEventListener("mouseout", function () {
+    image.style.border = "none";
+});
